d1077ff0ad3ece52eb8e327b723c5eb063b3ad8d
Lev <tyanlev1989@gmail.com>
2022-06-24 13:36:43 +0500

    SOURCE of the Marvel Project

---
+++ src/components/app/App.js
@@ -0,0 +1,44 @@
+import AppHeader from '../appHeader/AppHeader';
+import { CharInfo } from '../charInfo/CharInfo';
+import { CharList } from '../charList/CharList';
+import { RandomChar } from '../randomChar/RandomChar';
+
+import decoration from '../../resources/img/vision.png';
+import { Component } from 'react';
+import { ErrorBoundary } from '../errorBoundary/ErrorBoundary';
+
+export class App extends Component {
+  state = {
+    selectedChar: null,
+  };
+
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id,
+    });
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <AppHeader />
+        <main>
+          <ErrorBoundary>
+            <RandomChar />
+          </ErrorBoundary>
+
+          <div className='char__content'>
+            <ErrorBoundary>
+              <CharList onCharSelected={this.onCharSelected} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={this.state.selectedChar} />
+            </ErrorBoundary>
+          </div>
+
+          <img className='bg-decoration' src={decoration} alt='vision' />
+        </main>
+      </div>
+    );
+  }
+}
---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,17 @@
+import img from './error.gif';
+
+export const ErrorMessage = () => {
+  return (
+    <img
+      style={{
+        display: 'block',
+        width: '250px',
+        height: '250px',
+        objectFit: 'contain',
+        margin: '0 auto',
+      }}
+      src={img}
+      alt='Error'
+    />
+  );
+};
