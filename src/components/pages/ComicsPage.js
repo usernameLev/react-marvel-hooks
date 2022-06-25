e026068cb6851d26257bf028a59b983ef72204c0
Lev <tyanlev1989@gmail.com>
2022-06-25 08:10:28 +0500

    Create a react router

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,11 @@
+import { AppBanner } from '../appBanner/AppBanner';
+import { ComicsList } from '../comicsList/ComicsList';
+
+export const ComicsPage = () => {
+  return (
+    <>
+      <AppBanner />
+      <ComicsList />
+    </>
+  );
+};
