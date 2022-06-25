78ca9cc9b1a72538e27cc212946b9ebff3d72be5
Lev <tyanlev1989@gmail.com>
2022-06-25 11:30:27 +0500

    Make dynamic imports

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -8,7 +8,7 @@ import { Spinner } from '../spinner/Spinner';
 
 import './singleComicPage.scss';
 
-export const SingleComicPage = () => {
+const SingleComicPage = () => {
   const { comicId } = useParams();
 
   const [comic, setComic] = useState(null);
@@ -61,3 +61,5 @@ const View = ({ comic }) => {
     </div>
   );
 };
+
+export default SingleComicPage;
